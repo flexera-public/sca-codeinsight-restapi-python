c1155e0c17c4ab4427a1e00d30b2b7c7c69c818f
Alex Rybak <arybak@flexera.com>
2020-09-16 07:51:54 -0700

    Added support for get component details REST API call

---
+++ components/get_component_details.py
@@ -0,0 +1,57 @@
+'''
+Copyright 2020 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : arybak  
+Created On : Tues September 15 2020
+File : get_component_details.py
+'''
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+#------------------------------------------------------------------------------------------#
+def get_component_details(baseURL, componentID, authToken):
+    logger.info("Entering get_component_details")
+
+    RESTAPI_BASEURL = baseURL + "/codeinsight/api/"
+    ENDPOINT_URL = RESTAPI_BASEURL + "components/"
+    
+    RESTAPI_URL = ENDPOINT_URL + str(componentID) 
+    logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)
+    
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken} 
+       
+    ##########################################################################   
+    # Make the REST API call with the project data           
+    try:
+        response = requests.get(RESTAPI_URL, headers=headers)
+    except requests.exceptions.RequestException as error:  # Just catch all errors
+        logger.error(error)
+        return
+        
+    
+    ###############################################################################
+    # We at least received a response from FNCI so check the status to see
+    # what happened if there was an error or the expected data
+    if response.status_code == 200:
+        logger.info("    Componenet details received")
+        COMPONENT = (response.json())
+        return COMPONENT
+    elif response.status_code == 400:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Bad Request" %response.status_code )
+        response.raise_for_status()
+    elif response.status_code == 401:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Unauthorized" %response.status_code )
+        response.raise_for_status() 
+    elif response.status_code == 404:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Not Found" %response.status_code )
+        response.raise_for_status()   
+    else: 
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        response.raise_for_status()
