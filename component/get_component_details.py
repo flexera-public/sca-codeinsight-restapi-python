8faa962db78599edb2c7bfe48989230ed75d3c34
Sean Geary <sgeary@flexera.com>
2021-02-08 09:41:39 -0500

    Add v3 summary option for component details

--- component/get_component_details.py
+++ component/get_component_details.py
@@ -55,3 +55,48 @@ def get_component_details(baseURL, componentID, authToken):
     else: 
         logger.error("Response code %s - %s" %(response.status_code, response.text))
         response.raise_for_status()
+
+#------------------------------------------------------------------------------------------#
+def get_component_details_v3_summary(baseURL, componentID, authToken):
+    logger.info("Entering get_component_details_v3_summary")
+
+    RESTAPI_BASEURL = baseURL + "/codeinsight/api/"
+    ENDPOINT_URL = RESTAPI_BASEURL + "components/"
+    
+    RESTAPI_URL = ENDPOINT_URL + str(componentID) 
+    RESTAPI_URL = RESTAPI_URL + "?vulnerabilitySummary=true&cvssVersion=V3"
+    logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)
+    
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken} 
+       
+    ##########################################################################   
+    # Make the REST API call with the project data           
+    try:
+        response = requests.get(RESTAPI_URL, headers=headers)
+    except requests.exceptions.RequestException as error:  # Just catch all errors
+        logger.error(error)
+        return
+        
+    
+    ###############################################################################
+    # We at least received a response from FNCI so check the status to see
+    # what happened if there was an error or the expected data
+    if response.status_code == 200:
+        logger.info("    Componenet details received")
+        COMPONENT = (response.json())
+        return COMPONENT
+    elif response.status_code == 400:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Bad Request" %response.status_code )
+        response.raise_for_status()
+    elif response.status_code == 401:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Unauthorized" %response.status_code )
+        response.raise_for_status() 
+    elif response.status_code == 404:
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        print("Response code: %s   -  Not Found" %response.status_code )
+        response.raise_for_status()   
+    else: 
+        logger.error("Response code %s - %s" %(response.status_code, response.text))
+        response.raise_for_status()
