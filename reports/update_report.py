bd19a63629f0ab3ecf7f94bf53f1fbe191fc8bf3
Sean Geary <sgeary@flexera.com>
2021-05-29 16:38:34 -0400

    Add update report support

--- reports/update_report.py
+++ reports/update_report.py
@@ -1,61 +1,70 @@
 '''
-Copyright 2020 Flexera Software LLC
+Copyright 2021 Flexera Software LLC
 See LICENSE.TXT for full license text
 SPDX-License-Identifier: MIT
 
 Author : sgeary  
-Created On : Sun Aug 16 2020
-File : get_reports.py
+Created On : Fri May 28 2021
+File : update_report.py
 '''
+
 import logging
 import requests
+import json
 
 logger = logging.getLogger(__name__)
 
 #------------------------------------------------------------------------------------------#
-def get_all_currently_registered_reports(baseURL, authToken):
-
-    APIOPTIONS = ""
-    currentReports = get_currently_registered_reports(baseURL, authToken, APIOPTIONS)
-
-    return currentReports
-
-#------------------------------------------------------------------------------------------#
-def get_all_currently_registered_reports_by_name(baseURL, authToken, reportName):
-
-    APIOPTIONS = "?name=" + reportName
-    currentReports = get_currently_registered_reports(baseURL, authToken, APIOPTIONS)
-
-    return currentReports
-
-
-
-
-#------------------------------------------------------------------------------------------#
-def get_currently_registered_reports(baseURL, authToken, APIOPTIONS):
-    logger.info("Entering upload_project_report_data")
+def update_custom_report(reportName, reportPath, reportID, reportOrder, enableProjectPickerValue, reportOptions, baseURL, authToken):
+    logger.info("Entering update_custom_report")
 
     RESTAPI_BASEURL = baseURL + "/codeinsight/api/"
     ENDPOINT_URL = RESTAPI_BASEURL + "reports/"
-    RESTAPI_URL = ENDPOINT_URL + APIOPTIONS
+    RESTAPI_URL = ENDPOINT_URL
     logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)
-    
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken}   
+
+    #Take the list of reportOptions and create a string
+    if len(reportOptions):
+        optionsString = ""
+        for option in reportOptions:
+            optionsString += json.dumps(option) +","
+
+        optionsString = optionsString[:-1]
+
+    else:
+        optionsString = ""
+
+
+    createReportBody = '''
+    {
+        "id" : "''' + str(reportID) + '''",
+        "name": "''' + reportName + '''",
+        "path": "''' + reportPath + '''",
+        "order": "''' + str(reportOrder) + '''",
+        "enabled": "true",
+        "enableProjectPicker": "''' + enableProjectPickerValue + '''",
+        "reportOptions" : [''' + optionsString + ''']
+    }'''
+   
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken}     
        
     ##########################################################################   
     # Make the REST API call with the project data           
     try:
-        response = requests.get(RESTAPI_URL, headers=headers)
+        response = requests.put(RESTAPI_URL, headers=headers, data=createReportBody)
         logger.info("    Current report list retreived")
     except requests.exceptions.RequestException as error:  # Just catch all errors
+        print(response)
         logger.error(error)
-        raise
-        
+        #return
+
     ###############################################################################
     # We at least received a response from Code Insight so check the status to see
     # what happened if there was an error or the expected data
     if response.status_code == 200:
-        return(response.json()["data"])
+        logger.debug("%s was sucessfully updated" %(reportName))
+        return
+
     elif response.status_code == 400:
         logger.error("Response code %s - %s" %(response.status_code, response.text))
         print("Response code: %s   -  Bad Request" %response.status_code )
