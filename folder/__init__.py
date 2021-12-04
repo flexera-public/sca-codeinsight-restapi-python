c529423d84d0477c74030cb2a159c00dbc88ccb0
Sean Geary <sgeary@flexera.com>
2021-12-04 08:09:46 -0500

    Add API to create project folders

---
+++ folder/__init__.py
@@ -0,0 +1,9 @@
+'''
+Copyright 2021 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary  
+Created On : Fri Dec 03 2021
+File : __init__.py
+'''
