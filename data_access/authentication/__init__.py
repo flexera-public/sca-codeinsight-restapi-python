49b49c09942e8c7c8a578b28c590129b7cbbe467
Sean Geary <sgeary@flexera.com>
2022-01-17 18:21:21 -0500

    Add license data access APIs

---
+++ data_access/authentication/__init__.py
@@ -0,0 +1,9 @@
+'''
+Copyright 2022 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary  
+Created On : Sat Jan 15 2022
+File : __init__.py
+'''
