ff443d230939409eca4daccf7fec6fd9caf4025a
Sean Geary <sgeary@revenera.com>
2023-10-03 15:22:40 -0400

    Update component detail APIs

--- component/get_component_details.py
+++ component/get_component_details.py
@@ -15,56 +15,33 @@ logger = logging.getLogger(__name__)
 #------------------------------------------------------------------------------------------#
 def get_component_details(baseURL, componentID, authToken):
     logger.info("Entering get_component_details")
-
-    RESTAPI_BASEURL = baseURL + "/codeinsight/api/"
-    ENDPOINT_URL = RESTAPI_BASEURL + "components/"
-    
-    RESTAPI_URL = ENDPOINT_URL + str(componentID) 
-    logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)
-    
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken} 
-       
-    ##########################################################################   
-    # Make the REST API call with the project data           
-    try:
-        response = requests.get(RESTAPI_URL, headers=headers)
-    except requests.exceptions.RequestException as error:  # Just catch all errors
-        logger.error(error)
-        return
-        
-    
-    ###############################################################################
-    # We at least received a response from FNCI so check the status to see
-    # what happened if there was an error or the expected data
-    if response.status_code == 200:
-        logger.info("    Componenet details received")
-        COMPONENT = (response.json())
-        return COMPONENT
-    elif response.status_code == 400:
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Bad Request" %response.status_code )
-        response.raise_for_status()
-    elif response.status_code == 401:
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Unauthorized" %response.status_code )
-        response.raise_for_status() 
-    elif response.status_code == 404:
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Not Found" %response.status_code )
-        response.raise_for_status()   
-    else: 
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        response.raise_for_status()
+    APIOPTIONS = ""
+    componentDetails = get_component_details_with_options(baseURL, componentID, authToken, APIOPTIONS)
+    return componentDetails
 
 #------------------------------------------------------------------------------------------#
 def get_component_details_v3_summary(baseURL, componentID, authToken):
     logger.info("Entering get_component_details_v3_summary")
+    APIOPTIONS = "vulnerabilitySummary=true&cvssVersion=V3"
+    componentDetails = get_component_details_with_options(baseURL, componentID, authToken, APIOPTIONS)
+    return componentDetails
+
+#------------------------------------------------------------------------------------------#
+def get_component_details_without_versions(baseURL, componentID, authToken):
+    logger.info("Entering get_component_details_without_versions")
+    APIOPTIONS = "includeVersions=false"
+    componentDetails = get_component_details_with_options(baseURL, componentID, authToken, APIOPTIONS)
+    return componentDetails
+
+#------------------------------------------------------------------------------------------#
+def get_component_details_with_options(baseURL, componentID, authToken, APIOPTIONS):
+    logger.info("Entering get_component_details_with_options")
 
     RESTAPI_BASEURL = baseURL + "/codeinsight/api/"
     ENDPOINT_URL = RESTAPI_BASEURL + "components/"
-    
     RESTAPI_URL = ENDPOINT_URL + str(componentID) 
-    RESTAPI_URL = RESTAPI_URL + "?vulnerabilitySummary=true&cvssVersion=V3"
+    RESTAPI_URL += "?" + APIOPTIONS
+
     logger.debug("    RESTAPI_URL: %s" %RESTAPI_URL)
     
     headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken} 
@@ -75,28 +52,15 @@ def get_component_details_v3_summary(baseURL, componentID, authToken):
         response = requests.get(RESTAPI_URL, headers=headers)
     except requests.exceptions.RequestException as error:  # Just catch all errors
         logger.error(error)
-        return
-        
-    
+        return {"error" : error}
+   
     ###############################################################################
     # We at least received a response from FNCI so check the status to see
     # what happened if there was an error or the expected data
     if response.status_code == 200:
         logger.info("    Componenet details received")
-        COMPONENT = (response.json())
-        return COMPONENT
-    elif response.status_code == 400:
+        componentDetails = response.json()
+        return componentDetails
+    else:
         logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Bad Request" %response.status_code )
-        response.raise_for_status()
-    elif response.status_code == 401:
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Unauthorized" %response.status_code )
-        response.raise_for_status() 
-    elif response.status_code == 404:
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        print("Response code: %s   -  Not Found" %response.status_code )
-        response.raise_for_status()   
-    else: 
-        logger.error("Response code %s - %s" %(response.status_code, response.text))
-        response.raise_for_status()
+        return {"error" : response.text}
